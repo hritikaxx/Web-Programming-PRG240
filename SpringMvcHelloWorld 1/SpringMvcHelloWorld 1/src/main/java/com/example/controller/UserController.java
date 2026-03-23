242730a9af1448f39f021fb0d82d927a4228a48a
hritikaxx <hritika853singh@gmail.com>
2026-03-23 13:03:17 +0545

    week-2

--- SpringMvcHelloWorld 1/SpringMvcHelloWorld 1/src/main/java/com/example/controller/UserController.java
+++ SpringMvcHelloWorld 1/SpringMvcHelloWorld 1/src/main/java/com/example/controller/UserController.java
@@ -1,7 +1,7 @@
-package com.paws.controller;
+package com.example.controller;
 
 import com.example.service.UserService;
-import com.paws.model.User;
+import com.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

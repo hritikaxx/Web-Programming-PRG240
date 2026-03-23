d205df0459784f1b2f60f45039246f8f3fb1b925
hritikaxx <hritika853singh@gmail.com>
2026-03-23 07:08:06 +0545

    week-2

--- SpringMvcHelloWorld 1/SpringMvcHelloWorld 1/src/main/java/com/example/controller/UserController.java
+++ SpringMvcHelloWorld 1/SpringMvcHelloWorld 1/src/main/java/com/example/controller/UserController.java
@@ -8,22 +8,17 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class UserController {
 
-    // Open the registration page
     @GetMapping("/userregister")
     public String showRegistrationPage() {
-        return "userregistration";   // loads userregistration.jsp
+        return "userregistration";  
     }
 
-    // Handle form submission
     @PostMapping("/registerUser")
     public String registerUser(User user, Model model) {
 
-        // Here you could save the user to the database later
-        // For now we just send the user data to the summary page
-
         model.addAttribute("user", user);
 
-        return "usersummary";   // loads usersummary.jsp
+        return "usersummary";   
     }
 
 }
--- SpringMvcHelloWorld 1/SpringMvcHelloWorld 1/src/main/java/com/example/model/User.java
+++ SpringMvcHelloWorld 1/SpringMvcHelloWorld 1/src/main/java/com/example/model/User.java
@@ -4,7 +4,7 @@ public class User {
 
     private String fullName;
     private String email;
-    private String phone;
+    private String username;
     private String password;
 
     public String getFullName() { return fullName; }
@@ -13,8 +13,8 @@ public class User {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
